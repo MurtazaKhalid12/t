39a86d0c6cc546dc5c2cb539ef5cd9f971eed0ff
Murtazakhalid-BSCE22004 <112537271+Murtazakhalid-BSCE22004@users.noreply.github.com>
2023-02-12 03:40:40 +0500

    updated incluce.cpp

--- include.cpp
+++ include.cpp
@@ -1,11 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 int main()
 {
 
- 
- cout<<"mu name";
- 
+    cout << "khan";
+    cout << "mu name";
+
     return 0;
 }
